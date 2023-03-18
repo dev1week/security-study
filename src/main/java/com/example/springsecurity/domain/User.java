ef8f78e00b0d4f4653a694af7acfae5bd19798ef
dev1week <rondo2860@gmail.com>
2023-03-18 23:18:13 +0900

    #1 -3 회원 도메인 작성

---
+++ src/main/java/com/example/springsecurity/domain/User.java
@@ -0,0 +1,30 @@
+package com.example.springsecurity.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import java.sql.Timestamp;
+
+@Builder
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role; //ROLE_USER, ROLE_ADMIN
+
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
+
