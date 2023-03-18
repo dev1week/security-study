3c636e0eb07da3fe41331bee59cd117349a2429b
dev1week <rondo2860@gmail.com>
2023-03-18 23:21:06 +0900

    #1 -6  mvc config 작성
    - mustache 사용시 필요한 설정 추가

---
+++ src/main/java/com/example/springsecurity/Config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.example.springsecurity.Config;
+
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        MustacheViewResolver resolver = new MustacheViewResolver();
+
+        resolver.setCharset("UTF-8");
+        resolver.setContentType("text/html;charset=UTF-8");
+        resolver.setPrefix("classpath:/templates/");
+
+        //mustach가 .html을 인식하도록 설정 변경
+        resolver.setSuffix(".html");
+
+        registry.viewResolver(resolver);
+    }
+}
