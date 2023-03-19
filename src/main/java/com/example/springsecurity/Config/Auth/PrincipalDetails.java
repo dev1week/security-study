14887ebe429ff8abd23b230d86884c45145b41c9
dev1week <rondo2860@gmail.com>
2023-03-19 19:29:05 +0900

    #2 -9  PrincipalDetails에서 UserDetails와 OAuth2User 클래스를 전부 관리하도록 변경

--- src/main/java/com/example/springsecurity/Config/Auth/PrincipalDetails.java
+++ src/main/java/com/example/springsecurity/Config/Auth/PrincipalDetails.java
@@ -1,10 +1,13 @@
 package com.example.springsecurity.Config.Auth;
 import com.example.springsecurity.domain.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 //시큐리티가 / login 주소요청이 오면 낚아서 로그인을 진행시킨다.
 // 로그인 진행이 완료가 되면 시큐리티가 가지고 있는 session을 만들어준다. (Security ContextHolder라는 키를 갖는 세션을 찾으면 됨)
@@ -12,9 +15,21 @@ import java.util.Collection;
 //Authentication 안에 User 정보가 있어야한다.
 //User 오브젝트 타입 => UserDetails 타입 객체
 
+
 //시큐리티가 가지고 있는 세션 안에 세션정보를 넣을려면 Authentication 객체여야한다. => Authentication 안에 user를 저장할려면 UserDetails로 넣어줘야한다.
-public class PrincipalDetails implements UserDetails{
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
     private User user; //콤포지션
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
 
     public PrincipalDetails(User user){
         this.user = user;
@@ -57,5 +72,10 @@ public class PrincipalDetails implements UserDetails{
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
