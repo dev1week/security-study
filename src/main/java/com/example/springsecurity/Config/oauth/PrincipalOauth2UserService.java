32178116a17d14dba6226ebb357fba7e56712f7f
dev1week <rondo2860@gmail.com>
2023-03-19 19:27:43 +0900

    #2 -7 oauth 서비스에서 인자로 받는 userRequest 찍어보기

--- src/main/java/com/example/springsecurity/Config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/example/springsecurity/Config/oauth/PrincipalOauth2UserService.java
@@ -14,8 +14,12 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         System.out.println("userRequest: "+userRequest.getClientRegistration()); //registrationId로 어디서 로그인했는지 확인가능
         System.out.println("userRequest: "+userRequest.getAccessToken());
-        //구글 로그인 버튼 클릭 -> 구글 로그인 창 -> 로그인 완료 -> code 리턴 
+        //구글 로그인 버튼 클릭 -> 구글 로그인 창 -> 로그인 완료 -> code 리턴 (Oauth 클라이언트 라이브러리)->액세스 토큰 요청
+        // 유저 리퀘스트 정보 -> 회원 프로필 받아야함 (loadUser 함수) -> 구글로부터 회원 프로필 받아옴
         System.out.println("userRequest: "+super.loadUser(userRequest).getAttributes());
+
+        OAuth2User oauth2User = super.loadUser(userRequest);
+
 
         return super.loadUser(userRequest);
     }
