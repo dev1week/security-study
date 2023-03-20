455f521770091421808188008fcc8ce89bb1eee0
dev1week <rondo2860@gmail.com>
2023-03-20 15:43:26 +0900

    #2 -20 NaverUserInfo 구현

---
+++ src/main/java/com/example/springsecurity/Config/oauth/Provider/NaverUserInfo.java
@@ -0,0 +1,32 @@
+package com.example.springsecurity.Config.oauth.Provider;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuth2UserInfo{
+    //Oauth2User.getAttributes()를 메모리에 저장
+    private Map<String, Object> attributes;
+
+    public NaverUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProviderId() {
+        return String.valueOf(attributes.get("id"));
+    }
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getEmail() {
+        return String.valueOf(attributes.get("email"));
+    }
+
+    @Override
+    public String getName() {
+        return String.valueOf(attributes.get("name"));
+    }
+}
